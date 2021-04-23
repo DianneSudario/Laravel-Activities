d6e61a4a8cfd4d6210def31ded55c556fa0eb8bb
DianneSudario <201810784@gordoncollege.edu.ph>
2021-04-23 23:02:00 +0800

    init

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,19 +2,19 @@
 
 namespace App\Http\Controllers;
 
-
 use App\Models\Post;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-
     public function __construct()
-    {
+    {  
+        // Only authenticated user is allowed to create/update and delete a post
+        // Using construct function here in PostController
+        // It will protect all the routes from navigating in address bar
         $this->middleware('auth');
     }
-
 
     /**
      * Display a listing of the resource.
@@ -23,7 +23,8 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::get();
+        /** Dianne F. Sudario **/
+        $posts = Post::all();
         return view('posts.index', compact('posts'));
     }
 
@@ -34,6 +35,7 @@ class PostController extends Controller
      */
     public function create()
     {
+        /** Dianne F. Sudario **/
         return view('posts.create');
     }
 
@@ -45,78 +47,95 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
+        /** Dianne F. Sudario **/
+        $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'description' => 'required'
+        ]);
+        
+        if($request->hasfile('img')){
+
+            $filenameWithExt = $request->file('img')->getClientOriginalName();
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);  
+            $extension = $request->file('img')->getClientOriginalExtension();
+            $fileNameToStore = $filename.'_'.time().'.'.$extension;
+            $path = $request->file('img')->storeAs('public/img', $fileNameToStore);
+        } else {
+            $fileNameToStore = '';
+        }
+        
+        
+
+        //dd($request);
         $post = new Post();
-
-        $post->title = $request->title;
-
-        $post->description = $request->description;
-
-        $post->save();
-
-
-        return redirect('/posts');
+        $post->fill($request->all());
+        $post->img = $fileNameToStore;
+        if($post->save()){
+            $message = "Storing Data Successfully!";
+        }
+        
+        return redirect('/posts')->with('message', $message);
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        $post = Post::find($id);
-
+        /** Dianne F. Sudario **/
         return view('posts.show', compact('post'));
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Post $post)
     {
-        
-        $post = Post::find($id);
-
+        /** Dianne F. Sudario **/
         return view('posts.edit', compact('post'));
-
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
     {
+        $request->validate([
+            'title' => 'required|unique:posts|max:255',
+            'description' => 'required'
+        ]);
+        
+         /** Dianne F. Sudario **/
         $post = Post::find($id);
-
-        $post->title = $request->title;
-
-        $post->description = $request->description;
-
+        $post->fill($request->all());
         $post->save();
-
-
+        
         return redirect('/posts');
+        
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Post  $post
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
+        /** Dianne F. Sudario **/
         $post = Post::find($id);
         $post->delete();
-
+        
         return redirect('/posts');
     }
 }
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,61 +1,78 @@
 @extends('layouts.app')
 
-
 @section('content')
 
-    
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">       
-                <div class="card-header">New Post</div>
-                <div class="card-body">
-                 
-                   
-                        <form method="POST" action="{{ route('posts.store') }}">
-                            @csrf
-                            <div class="form-group row">
-                                <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title') }}</label>
-    
-                                <div class="col-md-6">
-                                    <input id="title" type="text" class="form-control" name="title" @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" required  autofocus>
-    
-                                    @error('title')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                            
-    
-                            <div class="form-group row">
-                                <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description') }}</label>
-    
-                                <div class="col-md-6">
-                                    <textarea class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" required autocomplete="description">
-                                    </textarea>
-                                    @error('description')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                              
-                            <div class="form-group row mb-0">
-                                <div class="col-md-6 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        {{ __('Submit') }}
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                     
-                </div>
-            </div>
-        </div>
-    </div>
+  <div class="row justify-content-center">
+      <div class="col-md-8">
+          <div class="card">       
+              <div class="card-header">NEW POST</div>
+              <div class="card-body">
+                
+                @if ($errors->any())
+                  <div class="alert alert-danger">
+                    <ul>
+                      @foreach ($errors->all() as $error)
+                          <li>{{ $error }}</li>
+                      @endforeach
+                    </ul>
+                  </div>
+                  @endif
+              
+                <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="form-group row">
+                      <label for="title" class="col-md-4 col-form-label text-md-right">{{ __('Title:') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="title" type="text" class="form-control @error('title') is-invalid @enderror" name="title" value="{{ old('title') }}" autofocus>
+                          
+                          @error('title')
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                      </div>
+                    </div>
+                    <div class="form-group row">
+                      <label for="description" class="col-md-4 col-form-label text-md-right">{{ __('Description:') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="description" type="text" class="form-control @error('description') is-invalid @enderror" name="description" value="{{ old('description') }}" autofocus>
+                          
+                          @error('description')
+                            <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                            </span>
+                          @enderror
+                      </div>
+                    </div>
+                    
+                    <div class="form-group row">
+                      <label for="img" class="col-md-4 col-form-label text-md-right">{{ __('Post Image:') }}</label>
+                      
+                      <div class="col-md-6">
+                        <input type="file" class="form-control-file @error('img') is-invalid @enderror" name="img">
+                      </div>
+                      @error('img')
+                        <span class="invalid-feedback" role="alert">
+                          <strong>{{ $message }}</strong>
+                        </span>
+                      @enderror
+                    </div>
+                    
+                    <div class="form-group row mb-0">
+                      <div class="col-md-6 offset-md-4">
+                          <button type="submit" class="btn btn-warning">
+                              Submit
+                          </button>
+                      </div>
+                  </div>
+                </form>
+              </div>
+          </div>
+      </div>
+  </div>
 </div>
-    
-@endsection 
+
+@endsection
